ae67389b6cbd706f762c99773390fbc96a082157
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-23 23:12:53 +0500

    add Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int a=1050;
+int b=21050;
+int c=14;
+int d=21050;
+int e=800;
+
+int max=a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+
+Console.Write("max= ");
+Console.Write(max);
